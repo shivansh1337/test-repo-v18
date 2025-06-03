451ded7b50b97c9c6af130d6d3c2c7f19008bb28
Shivansh Sharma <shivansh.report@gmail.com>
2025-06-03 20:50:18 +0530

    Upload template-code folder in a single commit

--- data/headerNavLinks.ts
+++
@@ -1,8 +0,0 @@
-const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/about', title: 'About' },
-]
-
-export default headerNavLinks
